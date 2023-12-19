7a9300e0bed9bcc996955ddde0f6f909e398e7fb
ZakariasNorde <nordeborgz@gmail.com>
2023-12-19 13:18:29 +0100

    projectController

---
+++ WebApplication1/Controllers/ProjectController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+namespace WebApplication1.Controllers
+{
+	public class ProjectController : Controller
+	{
+
+		public CVContext _service {  get; set; }
+		public ProjectController(CVContext service) 
+		
+		{
+			_service = service;
+		}
+
+		public IActionResult showProject()
+		{
+			return View();
+		}
+
+		public IActionResult AddProject()
+		{
+			return View();
+		}
+
+		public IActionResult EditProject()
+		{
+			return View();
+		}
+	}
+}
