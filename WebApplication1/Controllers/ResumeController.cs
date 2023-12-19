fc34db12cfa13574f0c225eb9a00e224d0674a55
PotRoastPiggy <alexmcc99@yahoo.se>
2023-12-19 13:11:18 +0100

    Updated CVContext and established the database

--- WebApplication1/Controllers/ResumeController.cs
+++ WebApplication1/Controllers/ResumeController.cs
@@ -2,14 +2,23 @@
 using WebApplication1.Models;
 namespace WebApplication1.Controllers
 {
-    
+
     public class ResumeController : Controller
     {
-        public ResumeController() { }
+        private CVContext _context;
+        public ResumeController(CVContext context) 
+        { 
+            _context = context;
+        }
         public IActionResult Search()
         {
+            var resumeList = from resume in _context.Resumes
+                             select resume;
 
-            return View();
+
+            
+            return View(resumeList.ToList());
         }
+
     }
 }
--- WebApplication1/Models/CVContext.cs
+++ WebApplication1/Models/CVContext.cs
@@ -1,12 +1,67 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
 
 namespace WebApplication1.Models
 {
     public class CVContext : DbContext
     {
-        public CVContext(DbContextOptions<CVContext> options) : base(options) 
-        {
+        public CVContext(DbContextOptions<CVContext> options) : base(options) { }
 
+        public DbSet<Resume> Resumes { get; set; }
+        public DbSet<Project> Projects { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Resume>().HasData(
+                new Resume
+                {
+                    Id = 1,
+                    Email = "Alexstuvsta@bingus.se",
+                    Qualification = new List<string> { "bingus", "bongus" },
+                    Phonenumber = new List<string> { "09348", "094854" },
+                    Education = new List<string> { "Harvard", "Yale" }
+
+                },
+                new Resume
+                {
+                    Id = 2,
+                    Email = "Alexstuvsta@bingus.se",
+                    Qualification = new List<string> { "bin", "bon" },
+                    Phonenumber = new List<string> { "09348999", "99094854" },
+                    Education = new List<string> { "Harvardle", "Yalebon" }
+
+                },
+                new Resume
+                {
+                    Id = 3,
+                    Email = "Alexstuvsta@bingus.se",
+                    Qualification = new List<string> { "binguruskus", "sibongus" },
+                    Phonenumber = new List<string> { "66609348", "666094854" },
+                    Education = new List<string> { "FakeHarvard", "RealYale" }
+
+                }
+                );
+            modelBuilder.Entity<Project>().HasData(
+                new Project 
+                {
+                    Id = 1,
+                    Title = "MIB",
+                    Description = "JAVA project",
+                    Created = DateTime.Now,
+                    Updated = DateTime.Now 
+                }
+                ,
+                new Project
+                {
+                    Id = 2,
+                    Title = "Hattmakaren",
+                    Description = "SCRUM Project",
+                    Created = DateTime.Now,
+                    Updated = DateTime.Now
+                }
+            );
         }
     }
 }
