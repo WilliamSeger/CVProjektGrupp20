ae211723b0efbc0ba73e19e568f3ff7deb794d21
ZakariasNorde <nordeborgz@gmail.com>
2023-12-20 10:29:55 +0100

    Small changes to project controller

--- WebApplication1/Controllers/ProjectController.cs
+++ WebApplication1/Controllers/ProjectController.cs
@@ -1,21 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebApplication1.Models;
 namespace WebApplication1.Controllers
 {
 	public class ProjectController : Controller
 	{
 
-		public CVContext _service {  get; set; }
-		public ProjectController(CVContext service) 
+		public CVContext _context {  get; set; }
+		public ProjectController(CVContext context) 
 		
 		{
-			_service = service;
+			_context = context;
 		}
 
 		public IActionResult showProject()
 		{
-			return View();
-		}
+            var projectList = from project in _context.Projects
+							  orderby project.Created descending
+                              select project;
+            return View(projectList.ToList());
+        }
 
 		public IActionResult AddProject()
 		{
