dc33c967267a13eec006edc04991915d7f383566
wbille <william.vir@hotmail.com>
2024-01-03 12:39:00 +0100

    New message model

---
+++ WebApplication1/Models/Message.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+	public class Message
+	{
+		public int Id { get; set; }
+		[Required]
+		public DateTime Created { get; set; }
+		[Required]
+		public string Content { get; set; }
+		[Required]
+		public bool IsRead { get; set; }
+		public int SenderId { get; set; }
+		[ForeignKey(nameof(SenderId))]
+		public virtual Profile Sender { get; set; }
+		public int RecieverId { get; set; }
+		[ForeignKey(nameof(RecieverId))]
+		public virtual Profile Reciever { get; set; }
+	}
+}
