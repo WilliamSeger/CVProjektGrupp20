5a7f38563b3d13cc38f57dd7fb67f743d5f06ba9
wbille <william.vir@hotmail.com>
2024-01-06 12:11:51 +0100

    Message notifications finito

---
+++ DAL/Migrations/20240106110118_MessageCount.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Migrations
+{
+    /// <inheritdoc />
+    public partial class MessageCount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MessagesCount",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "123",
+                columns: new[] { "ConcurrencyStamp", "MessagesCount", "SecurityStamp" },
+                values: new object[] { "5ba7a9fe-4bc5-470a-a57f-88482e9ebe29", 0, "e113e03c-a695-4cd4-b993-ab2458d9a130" });
+
+            migrationBuilder.UpdateData(
+                table: "Messages",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2024, 1, 6, 12, 1, 18, 530, DateTimeKind.Local).AddTicks(5893));
+
+            migrationBuilder.UpdateData(
+                table: "Messages",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Created",
+                value: new DateTime(2024, 1, 6, 12, 1, 18, 530, DateTimeKind.Local).AddTicks(5917));
+
+            migrationBuilder.UpdateData(
+                table: "Messages",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Created",
+                value: new DateTime(2024, 1, 6, 12, 1, 18, 530, DateTimeKind.Local).AddTicks(5919));
+
+            migrationBuilder.UpdateData(
+                table: "Messages",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Created",
+                value: new DateTime(2024, 1, 6, 12, 1, 18, 530, DateTimeKind.Local).AddTicks(5921));
+
+            migrationBuilder.UpdateData(
+                table: "Projects",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Created", "Updated" },
+                values: new object[] { new DateTime(2024, 1, 6, 12, 1, 18, 529, DateTimeKind.Local).AddTicks(3254), new DateTime(2024, 1, 6, 12, 1, 18, 529, DateTimeKind.Local).AddTicks(3299) });
+
+            migrationBuilder.UpdateData(
+                table: "Projects",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Created", "Updated" },
+                values: new object[] { new DateTime(2024, 1, 6, 12, 1, 18, 529, DateTimeKind.Local).AddTicks(3302), new DateTime(2024, 1, 6, 12, 1, 18, 529, DateTimeKind.Local).AddTicks(3303) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MessagesCount",
+                table: "AspNetUsers");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "123",
+                columns: new[] { "ConcurrencyStamp", "SecurityStamp" },
+                values: new object[] { "2a0bb15b-d734-4c4d-af81-30fcd783234b", "60ba611c-526d-4485-b647-a80eebd878e2" });
+
+            migrationBuilder.UpdateData(
+                table: "Messages",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2024, 1, 5, 13, 55, 16, 297, DateTimeKind.Local).AddTicks(1525));
+
+            migrationBuilder.UpdateData(
+                table: "Messages",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Created",
+                value: new DateTime(2024, 1, 5, 13, 55, 16, 297, DateTimeKind.Local).AddTicks(1577));
+
+            migrationBuilder.UpdateData(
+                table: "Messages",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Created",
+                value: new DateTime(2024, 1, 5, 13, 55, 16, 297, DateTimeKind.Local).AddTicks(1581));
+
+            migrationBuilder.UpdateData(
+                table: "Messages",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Created",
+                value: new DateTime(2024, 1, 5, 13, 55, 16, 297, DateTimeKind.Local).AddTicks(1584));
+
+            migrationBuilder.UpdateData(
+                table: "Projects",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Created", "Updated" },
+                values: new object[] { new DateTime(2024, 1, 5, 13, 55, 16, 292, DateTimeKind.Local).AddTicks(3684), new DateTime(2024, 1, 5, 13, 55, 16, 292, DateTimeKind.Local).AddTicks(3736) });
+
+            migrationBuilder.UpdateData(
+                table: "Projects",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Created", "Updated" },
+                values: new object[] { new DateTime(2024, 1, 5, 13, 55, 16, 292, DateTimeKind.Local).AddTicks(3740), new DateTime(2024, 1, 5, 13, 55, 16, 292, DateTimeKind.Local).AddTicks(3742) });
+        }
+    }
+}
--- WebApplication1/Controllers/MessageController.cs
+++ WebApplication1/Controllers/MessageController.cs
@@ -127,6 +127,18 @@ namespace WebApplication1.Controllers
             message.SenderId = senderProfile.Id;
             message.Reciever = recieverProfile;
             message.RecieverId = recieverProfile.Id;
+
+            User recieverUser = recieverProfile.User;
+
+            if (recieverUser.MessagesCount == null || recieverUser.MessagesCount == 0)
+            {
+                recieverUser.MessagesCount = 1;
+            }
+            else
+            {
+                recieverUser.MessagesCount = (recieverUser.MessagesCount + 1);
+            }
+
             if (message.Sender != null && message.Reciever != null)
             {
                 _context.Messages.Add(message);
@@ -157,6 +169,16 @@ namespace WebApplication1.Controllers
             message.RecieverId = recieverProfile.Id;
             message.SenderName = viewModel.SenderName;
 
+            User recieverUser = recieverProfile.User;
+            if (recieverUser.MessagesCount == null || recieverUser.MessagesCount == 0)
+            {
+                recieverUser.MessagesCount = 1;
+            }
+            else
+            {
+                recieverUser.MessagesCount = (recieverUser.MessagesCount + 1);
+            }
+
             if (message.Reciever != null)
             {
                 _context.anonMessages.Add(message);
@@ -185,6 +207,9 @@ namespace WebApplication1.Controllers
             }
 
             User user = await userManager.FindByNameAsync(User.Identity.Name);
+            user.MessagesCount = user.MessagesCount - 1;
+            _context.SaveChanges();
+
             var profileQuery = from profile in _context.Profiles
                                where profile.UserId == user.Id
                                select profile;
@@ -193,6 +218,11 @@ namespace WebApplication1.Controllers
             var recievedMessageQuery = from message in _context.Messages
                                        where message.RecieverId == userProfile.Id
                                        select message;
+
+            var anonMessageQuery = from anonMessage in _context.anonMessages
+                                   where anonMessage.RecieverId == userProfile.Id
+                                   select anonMessage;
+            ViewBag.AnonMessages = anonMessageQuery.ToList();
 
             List<Message> messages = recievedMessageQuery.ToList();
 
@@ -214,6 +244,9 @@ namespace WebApplication1.Controllers
             }
 
             User user = await userManager.FindByNameAsync(User.Identity.Name);
+            user.MessagesCount = user.MessagesCount - 1;
+            _context.SaveChanges();
+
             var profileQuery = from profile in _context.Profiles
                                where profile.UserId == user.Id
                                select profile;
